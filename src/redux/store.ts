18c91f579f637e041976c69bdfbfc75949d4fef4
tinkokowin96 <tinkokowin.96@gmail.com>
2022-01-05 03:26:37 +0630

    remove logger for production

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,12 @@
-import { applyMiddleware, createStore } from "@reduxjs/toolkit"
-import logger from "redux-logger"
+import { createStore } from "@reduxjs/toolkit"
+// import logger from "redux-logger"
 import { rootReducer } from "./root-reducer"
 
-const middlewares = []
-if (process.env.NODE_ENV === "development") {
-  middlewares.push(logger)
-}
+// const middlewares = []
+// if (process.env.NODE_ENV === "development") {
+//   middlewares.push(logger)
+// }
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares))
+export const store = createStore(rootReducer)
+// export const store = createStore(rootReducer, applyMiddleware(...middlewares))
 export type AppDispatch = typeof store.dispatch
