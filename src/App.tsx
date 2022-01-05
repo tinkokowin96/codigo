2c03fdbe5773ee78cf6e2ce42a20f00e1b9fa38d
tinkokowin96 <tinkokowin.96@gmail.com>
2022-01-05 06:35:32 +0630

    hamburger menu

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,43 @@ import Footer from "./components/footer"
 import Header from "./components/header"
 import Navigation from "./components/navigation"
 import Work from "./components/work"
+import styles from "./styles/App.module.scss"
 
 function App() {
-  console.log("Why....")
   const [redirect, setRedirect] = useState(false)
+  const [openMenu, setOpenMenu] = useState(false)
+  // const menuRef = useRef(null)
   if (redirect) {
     return <Details redirect={setRedirect} />
   }
+
   return (
-    <div className="App">
-      <Navigation redirect={null} showFeatures />
+    <div className={styles.App} data-open={openMenu && "true"}>
+      {/* <div className={styles.menuContainer} ref={menuRef}>
+        {openMenu && (
+          <div className={styles.menu}>
+            <div className={styles.close} onClick={closeMenuHandler}>
+              x
+            </div>
+            <div className={styles.actions}>
+              <div> Work</div>
+              <div> Solutions</div>
+              <div> Services</div>
+              <div> About us</div>
+              <div> Blog </div>
+              <div> Request a quote </div>
+              <div> Let&apos;s chat</div>
+            </div>
+            <div className={styles.icons}>
+              <img src="./icons/social-facebook_white.svg" alt="" />
+              <img src="./icons/social-twitter_white.svg" alt="" />
+              <img src="./icons/social-instagram_white.svg" alt="" />
+              <img src="./icons/social-linkedin_white.svg" alt="" />
+            </div>
+          </div>
+        )}
+      </div> */}
+      <Navigation redirect={null} showFeatures menu={{ open: openMenu, set: setOpenMenu }} />
       <Header />
       <Work redirect={setRedirect} />
       <Footer />
--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,14 +1,28 @@
+import { useRef } from "react"
 import styles from "../styles/Navigation.module.scss"
 
-export default function Navigation({
-  redirect,
-  showFeatures,
-}: {
+interface iNavigation {
   redirect: React.Dispatch<React.SetStateAction<boolean>> | null
   showFeatures: boolean
-}) {
+  menu?: {
+    open: boolean
+    set: React.Dispatch<React.SetStateAction<boolean>>
+  } | null
+}
+
+export default function Navigation({ redirect, showFeatures, menu = null }: iNavigation) {
+  const menuRef = useRef(null)
+
+  const clickMenuHandler = () => {
+    if (menu && !menu.open) {
+      menu.set(true)
+    } else if (menu && menu.open) {
+      menu.set(false)
+    }
+  }
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} data-open={menu && menu.open && "true"}>
       {showFeatures && <img className={styles.logo} src="./icons/logo.svg" alt="" />}
       {!showFeatures && redirect && (
         <div className={styles.backWork} onClick={() => redirect(false)}>
@@ -16,6 +30,33 @@ export default function Navigation({
           <div>Back to work</div>
         </div>
       )}
+      <div className={styles.hamMenu} ref={menuRef} data-open={menu && menu.open && "true"}>
+        <div className={styles.hamburger} onClick={clickMenuHandler}>
+          <div className={styles.humItem}></div>
+          <div className={styles.humItem}></div>
+          <div className={styles.humItem}></div>
+        </div>
+
+        {menu?.open && (
+          <div className={styles.menu}>
+            <div className={styles.openActions}>
+              <div> Work</div>
+              <div> Solutions</div>
+              <div> Services</div>
+              <div> About us</div>
+              <div> Blog </div>
+              <div> Request a quote </div>
+              <div> Let&apos;s chat</div>
+            </div>
+            <div className={styles.icons}>
+              <img src="./icons/social-facebook_white.svg" alt="" />
+              <img src="./icons/social-twitter_white.svg" alt="" />
+              <img src="./icons/social-instagram_white.svg" alt="" />
+              <img src="./icons/social-linkedin_white.svg" alt="" />
+            </div>
+          </div>
+        )}
+      </div>
       <div className={styles.actions}>
         {showFeatures && (
           <>
